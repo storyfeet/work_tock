63ae0c528d75415cf71a5395fe111e8ecfb2742f
storyfeet <thebeaky@psmail.net>
2019-01-11 15:00:41 +0000

    weeks now work on filter

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use std::collections::BTreeMap;
 
+use chrono::naive::NaiveDate;
+use chrono::offset::Local;
+use chrono::{Datelike, Weekday};
+
 mod clockin;
 use crate::clockin::Clockin;
 mod parse;
@@ -12,6 +16,7 @@ fn main() -> Result<(), String> {
     let mut cfg = lazy_conf::config("-c", &["{HOME}/.config/work_tock/init"])
         .map_err(|_| "Wierd Arguments")?;
 
+    //core options
     let fname = cfg
         .grab()
         .fg("-f")
@@ -19,60 +24,82 @@ fn main() -> Result<(), String> {
         .help("Filename: what file to look at")
         .s();
 
+    //general options
+
+    let week = cfg
+        .grab()
+        .fg("-wk")
+        .help("Week Of Year: 1 to 53 or use '-' for this week")
+        .s();
+
     if cfg.help("Work Tock") {
         return Ok(());
     }
 
-    let fname = lazy_conf::env::replace_env(&fname.ok_or("No Filename provided use -f")?).map_err(|_|"could not env replace")?;
-    
+    let fname = lazy_conf::env::replace_env(&fname.ok_or("No Filename provided use -f")?)
+        .map_err(|_| "could not env replace")?;
 
-    let s = std::fs::read_to_string(&fname).map_err(|_| format!("Could not read file: {}",fname))?;
-
-
+    let s =
+        std::fs::read_to_string(&fname).map_err(|_| format!("Could not read file: {}", fname))?;
 
     let (clocks, errs) = clockin::read_string(&s);
 
     println!("\n\nERRS  \n{:?}", errs);
 
-    let mut curr= None;
+    let mut curr = None;
     let mut c_io = Vec::new();
     //Get outs with ins so filter makes sense
     for c in clocks {
         match c {
-            Clockin::In(data)=>{
-                if let Some(cin) = curr{
-                    c_io.push((cin,data.time));
+            Clockin::In(data) => {
+                if let Some(cin) = curr {
+                    c_io.push((cin, data.time));
                 }
                 curr = Some(data);
             }
-            Clockin::Out(cout)=>{
+            Clockin::Out(cout) => {
                 match curr {
-                    Some(data)=>c_io.push((data,cout)),
-                    None=>println!("Two Out's in a row"),
+                    Some(data) => c_io.push((data, cout)),
+                    None => println!("Two Out's in a row"),
                 }
                 curr = None;
             }
         }
     }
-    if let Some(data) = curr{
-        c_io.push((data,STime::now()));
+    if let Some(data) = curr {
+        c_io.push((data, STime::now()));
     }
 
     //TODO add filters.
+    if let Some(wks) = week {
+        let dt = Local::today();
+        let (st, fin) = match wks.parse::<u32>() {
+            Ok(n) => (
+                NaiveDate::from_isoywd(dt.year(), n, Weekday::Mon),
+                NaiveDate::from_isoywd(dt.year(), n, Weekday::Sun),
+            ),
+            Err(_) => (
+                NaiveDate::from_isoywd(dt.year(), dt.iso_week().week(), Weekday::Mon),
+                NaiveDate::from_isoywd(dt.year(), dt.iso_week().week(), Weekday::Sun),
+            ),
+        };
+        c_io.retain(|(ind, _)| ind.date >= st && ind.date <= fin);
+    }
 
     //build report
-    let mut r_times:BTreeMap<String,STime> = BTreeMap::new();
-    let mut t_time = STime::new(0,0);
-    for (idat,otime) in c_io{
-        let tt = r_times.get(&idat.job).map(|x|*x).unwrap_or(STime::new(0,0));
-        r_times.insert(idat.job,tt + otime - idat.time);
+    let mut r_times: BTreeMap<String, STime> = BTreeMap::new();
+    let mut t_time = STime::new(0, 0);
+    for (idat, otime) in c_io {
+        let tt = r_times
+            .get(&idat.job)
+            .map(|x| *x)
+            .unwrap_or(STime::new(0, 0));
+        r_times.insert(idat.job, tt + otime - idat.time);
         t_time += otime - idat.time;
     }
 
-
-    println!("{:?}",r_times);
-    println!("Total Time = {}",t_time);
-
+    println!("{:?}", r_times);
+    println!("Total Time = {}", t_time);
 
     Ok(())
 }
